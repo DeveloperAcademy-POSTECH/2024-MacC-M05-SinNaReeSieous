11916847b3e250a372be4c6f1cd2bdab977d6e3b
woboneyo <bluewing1004@gmail.com>
2024-10-15 18:33:18 +0900

    [SETTINGS/#9] 프로젝트파일 재생성

---
+++ Eggong/Eggong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Eggong
+//
+//  Created by 조우현 on 10/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Eggong/Eggong/EggongApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggongApp.swift
+//  Eggong
+//
+//  Created by 조우현 on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct EggongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
