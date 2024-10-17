8d286c7442659d25865c265a37b888f6184a1979
WooFeather <bluewing1004@gmail.com>
2024-10-17 18:09:27 +0900

    [Setting] #3 - 네트워크 폴더 설정

---
+++ Eggong/Eggong/Data/Network/FirebaseFoundation/FirebaseFoundation.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseFoundation.swift
+//  Eggong
+//
+//  Created by 조우현 on 10/17/24.
+//
+
+import Foundation
---
+++ Eggong/Eggong/Data/Network/Service/PlaceService.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceService.swift
+//  Eggong
+//
+//  Created by 조우현 on 10/17/24.
+//
+
+import Foundation
