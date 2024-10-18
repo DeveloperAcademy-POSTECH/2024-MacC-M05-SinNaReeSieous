6200fb1c7eab17d6e894cf58b11aadd15752fd23
YunHak Lee <lyh990408@gmail.com>
2024-10-18 12:43:52 +0900

    [Network] #5 - Firebase를 프로젝트 파일과 연결

--- Eggong/Eggong/App/EggongApp.swift
+++ Eggong/Eggong/App/EggongApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct EggongApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
