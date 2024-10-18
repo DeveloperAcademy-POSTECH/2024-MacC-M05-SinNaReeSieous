2f0ae6197daf6c599cfa8dff187a217cd411291e
YunHak Lee <lyh990408@gmail.com>
2024-10-18 19:23:47 +0900

    [Network] #5 - 이미지 여러장 post하는 경우 동시에 비동기 작업이 이뤄지는 메서드 구현

--- Eggong/Eggong/Data/Network/FirebaseFoundation/Storage/FirebaseStorageFoundation.swift
+++ Eggong/Eggong/Data/Network/FirebaseFoundation/Storage/FirebaseStorageFoundation.swift
@@ -19,7 +19,7 @@ protocol FirebaseStorageFoundation {
 extension FirebaseStorageFoundation {
     
     /// UIImagePicker를 사용할 때 사용하기 편하게 이미지를 업로드 할 수 있는
-    func postUIImage(path: String, imageName: String, _ image: UIImage?) async throws -> String {
+    func postUIImage(path: String, imageName: String, image: UIImage?) async throws -> String {
         guard let image else {
             throw FirebaseError.dataNotFound
         }
@@ -32,13 +32,33 @@ extension FirebaseStorageFoundation {
     }
     
     /// SwiftUI용으로 새로 나온 PhotosPicker 를 사용할 때 편하게 이미지를 업로드 할 수 있는 함수
-    func postPhotosPickerItem(path: String, imageName: String, _ item: PhotosPickerItem) async throws -> String {
+    func postPhotosPickerItem(path: String, imageName: String, item: PhotosPickerItem) async throws -> String {
         guard let data = try? await item.loadTransferable(type: Data.self) else {
             throw FirebaseError.encodingFailed
         }
         
         let imagePath = "\(path)/\(imageName).jpg"
         return try await postData(path: imagePath, data: data)
+    }
+    
+    /// 여러 PhotosPickerItem을 동시에 업로드 할때. post 코드는 병렬로 호출되어서 동시에 진행된다.
+    func postPhotosPickerItems(path: String, imageName: String, items: [PhotosPickerItem]) async throws -> [String] {
+        var results = Array(repeating: "", count: items.count)
+        
+        try await withThrowingTaskGroup(of: (Int, String).self) { group in
+            for (index, item) in items.enumerated() {
+                group.addTask {
+                    let url = try await postPhotosPickerItem(path: path, imageName: imageName, item: item)
+                    return (index, url)
+                }
+            }
+            
+            for try await (index, url) in group {
+                results[index] = url
+            }
+        }
+        
+        return results
     }
     
     /// 범용적으로 Data 타입의 값을 업로드 할 수 있는 함수, 다른 메서드를 위한 기초 함수. 다운로드 URL을 반환한다.
