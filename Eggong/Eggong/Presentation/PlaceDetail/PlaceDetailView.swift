a94f9b842c6992561c55471b87ca8b5d83c5c349
WooFeather <bluewing1004@gmail.com>
2024-10-19 23:13:48 +0900

    [Design] #6 - PlaceDetailHeaderView작업
    더미로 들어가있는 사진, 텍스트들은 #5-2 PR이 머지된 이후, 실제 데이터로 교체될 예정

--- Eggong/Eggong/Presentation/PlaceDetail/PlaceDetailView.swift
+++ Eggong/Eggong/Presentation/PlaceDetail/PlaceDetailView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct PlaceDetailView: View {
     
-    // MARK: - Body
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    // MARK: Body
     
     var body: some View {
         VStack {
@@ -19,14 +21,59 @@ struct PlaceDetailView: View {
             storyView
             footerView
         }
+        .ignoresSafeArea()
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                NavigationBackButton {
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    // TODO: 신고하기 View로 이동
+                } label: {
+                    Image(systemName: "light.beacon.min")
+                }
+            }
+        }
+        .gesture(DragGesture().onEnded { gesture in
+            if gesture.translation.width > 100 {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        })
     }
 }
 
 private extension PlaceDetailView {
-    // MARK: - View
+    // MARK: View
     
     var headerView: some View {
-        PlaceDetailHeaderView()
+        ZStack(alignment: .bottomLeading) {
+            Image(.sampleThumbnail)
+                .resizable()
+                .scaledToFill()
+                .frame(maxHeight: 340)
+                .clipped()
+                .overlay {
+                    Rectangle()
+                        .fill(Color.black.opacity(0.3))
+                }
+            
+            VStack(alignment: .leading) {
+                Text("카페 휙")
+                    .font(.largeTitle.bold())
+                
+                HStack(spacing: 20) {
+                    Text("따뜻한")
+                    Text("자유로운")
+                    Text("풋풋한")
+                }
+                .font(.caption)
+            }
+            .padding(16)
+        }
     }
     
     var descriptionView: some View {
