e21348b88f7ea8361f652ec7d912910df2f9483f
WooFeather <bluewing1004@gmail.com>
2024-10-20 00:37:27 +0900

    [Design] #6 - FooterView 작업

--- Eggong/Eggong/Presentation/PlaceDetail/PlaceDetailAssistantView/PlaceDetailFooterView.swift
+++ Eggong/Eggong/Presentation/PlaceDetail/PlaceDetailAssistantView/PlaceDetailFooterView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct PlaceDetailFooterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("더 자세한 정보")
+                .font(.title2.bold())
+                .padding(.bottom, 10)
+            
+            HStack {
+                Text("네이버지도")
+                    .font(.caption.bold())
+                
+                Spacer()
+                
+                Button {
+                    // TODO: 네이버지도로 이동
+                } label: {
+                    Text("바로가기").underline()
+                        .foregroundStyle(.gray)
+                        .font(.caption.bold())
+                }
+            }
+            
+            HStack {
+                Text("카카오맵")
+                    .font(.caption.bold())
+                
+                Spacer()
+                
+                Button {
+                    // TODO: 카카오맵으로 이동
+                } label: {
+                    Text("바로가기").underline()
+                        .foregroundStyle(.gray)
+                        .font(.caption.bold())
+                }
+            }
+        }
+        .padding(16)
     }
 }
 
