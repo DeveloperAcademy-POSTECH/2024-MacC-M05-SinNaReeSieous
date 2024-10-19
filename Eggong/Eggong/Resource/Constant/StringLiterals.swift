fdb3a3355d08962cf93011a5025570ac094ebe35
YunHak Lee <lyh990408@gmail.com>
2024-10-19 12:54:49 +0900

    [Network] #5-2 - Firebase 통신을 위한 Path들 정리

--- Eggong/Eggong/Resource/Constant/StringLiterals.swift
+++ Eggong/Eggong/Resource/Constant/StringLiterals.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum StringLiterals {
+    enum Network {
+        // Place
+        static let placePath = "Places"
+        static func placeImagePath(id: String) -> String {
+            return "\(placePath)/\(id)"
+        }
+        
+        // PlaceDetail
+        static let placeDetailPath = "PlaceDetails"
+        static func placeDetailImagePath(id: String) -> String {
+            return "\(placeDetailPath)/\(id)"
+        }
+        
+        //User
+        static let userPath = "Users"
+        static let dummyUserID = "262179B1-2AD6-416B-9323-E62C5A19D0A9"
+        static func userImagePath(id: String) -> String {
+            return "\(userPath)/\(id)"
+        }
+    }
+}
