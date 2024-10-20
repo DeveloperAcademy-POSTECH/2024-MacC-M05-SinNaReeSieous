f3b2acc8975250059a02c2d4f4c23d1bd700c67c
Woohyun Jo <bluewing1004@gmail.com>
2024-10-20 17:48:10 +0900

    [Design] #6 - PlaceDetailView UI 구현 완료
    PlaceDetailAssistantView들은 필요없으면 삭제 예정

--- Eggong/Eggong/Presentation/PlaceDetail/PlaceDetailAssistantView/PlaceDetailView.swift
+++ Eggong/Eggong/Presentation/PlaceDetail/PlaceDetailAssistantView/PlaceDetailView.swift
@@ -66,6 +66,7 @@ struct PlaceDetailView: View {
         })
         .task {
             do {
+                // TODO: getPlaceDetail id 처리
                 self.placeDetail = try await placeDetailService.getPlaceDetail(id: placeDetail.id) ?? PlaceDetail.dummy
             } catch {
                 print(error)
@@ -79,7 +80,7 @@ private extension PlaceDetailView {
     
     var headerView: some View {
         ZStack(alignment: .bottomLeading) {
-            Image(.sampleThumbnail)
+            KFImage(URL(string: placeDetail.imageURLString))
                 .resizable()
                 .scaledToFill()
                 .frame(height: max(340 + scrollOffset, 340))
@@ -90,17 +91,13 @@ private extension PlaceDetailView {
                 }
             
             VStack(alignment: .leading) {
-                Text("카페 휙")
+                Text(placeDetail.name)
                     .font(.largeTitle.bold())
                     .foregroundStyle(.white)
                 
-                HStack(spacing: 20) {
-                    Text("따뜻한")
-                    Text("자유로운")
-                    Text("풋풋한")
-                }
-                .foregroundStyle(.white)
-                .font(.caption)
+                Text(keywordText)
+                    .foregroundStyle(.white)
+                    .font(.caption)
             }
             .padding(16)
         }
@@ -116,7 +113,7 @@ private extension PlaceDetailView {
             }
             .font(.title2.bold())
             
-            Text("저는 이 카페가 사람들이 와서 편하게 수다 떨고, 웃고, 함께 시간을 보낼 수 있는 공간이었으면 좋겠어요. 커피나 음료는 물론 중요하지만, 이곳은 대화하고 어울리는 게 더 큰 의미라고 생각하거든요. 혼자 조용히 책을 읽는 그런 카페가 아니라, 친구들끼리 모여서 시끌벅적하게 이야기 나누고, 웃음소리로 가득 찬 그런 분위기를 만들고 싶어요.")
+            Text(placeDetail.makingStory)
                 .padding(.top, 8)
         }
         .padding(.horizontal, 16)
@@ -130,19 +127,19 @@ private extension PlaceDetailView {
             
             GeometryReader { geometry in
                 TabView(selection: $selectedIndex) {
-                    Image(.smapleImage1)
+                    KFImage(URL(string: placeDetail.storyImageURLStrings[0]))
                         .resizable()
                         .scaledToFill()
                         .frame(width: geometry.size.width, height: geometry.size.height)
                         .clipped()
                         .tag(0)
-                    Image(.smapleImage2)
+                    KFImage(URL(string: placeDetail.storyImageURLStrings[1]))
                         .resizable()
                         .scaledToFill()
                         .frame(width: geometry.size.width, height: geometry.size.height)
                         .clipped()
                         .tag(1)
-                    Image(.smapleImage3)
+                    KFImage(URL(string: placeDetail.storyImageURLStrings[2]))
                         .resizable()
                         .scaledToFill()
                         .frame(width: geometry.size.width, height: geometry.size.height)
@@ -159,11 +156,11 @@ private extension PlaceDetailView {
     var storyView: some View {
         ZStack {
             if selectedIndex == 0 {
-                Text("지난달에는 고려대 학생들과 협업해 아침밥 사업을 기획하고 진행했어요. 등교하는 학생들, 출근하는 직원들, 그리고 고생한다며 따뜻한 인사를 건네주시는 어르신들 덕분에 마음이 참 따뜻해졌답니다. 여섯 시부터 출근하느라 피곤했지만, 그만큼 보람도 크게 느꼈어요.")
+                Text(placeDetail.stories[0])
             } else if selectedIndex == 1 {
-                Text("리브랜딩을 위해 새벽까지 이어지는 메뉴 개발 현장입니다. 휙의 시그니처 메뉴들은 긴 시간에 걸쳐 내부 회의와 수많은 시음을 거쳐 손님들께 선보이게 됩니다. 한층 더 완성도 높은 메뉴를 위해 팀원들은 끊임없는 논의와 테스트를 반복하며 최선을 다하고 있습니다.")
+                Text(placeDetail.stories[1])
             } else if selectedIndex == 2 {
-                Text("최근에는 시립대 관현악 동아리와 협업해 특별한 클래식 공연을 열었습니다. 이번엔시립대 학생들뿐만 아니라 동네 주민분들도 함께 찾아주셔서 모두가 한데 어우러져 클래식의 매력을 만끽한 멋진 경험이었어요. 지역 사회와 더 가까워질 수 있는 뜻깊은 시간이었습니다.")
+                Text(placeDetail.stories[2])
             }
         }
         .transition(.opacity)
@@ -213,6 +210,13 @@ private extension PlaceDetailView {
                 .padding(.bottom, 30)
         }
         .padding(.horizontal, 16)
+    }
+    
+    // MARK: Computed Values
+    
+    var keywordText: String {
+        let sortedKeywords = placeDetail.keywords.sorted(by: {$0.count < $1.count})
+        return sortedKeywords.joined(separator: "\t")
     }
 }
 
