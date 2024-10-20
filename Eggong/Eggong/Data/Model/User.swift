2fe954c5e64720cfc3adefb157f77fe24f07a7d5
YunHak Lee <lyh990408@gmail.com>
2024-10-20 16:15:19 +0900

    [Network] #5-2 - User, Place 더미 데이터 구조체에 추가

--- Eggong/Eggong/Data/Model/User.swift
+++ Eggong/Eggong/Data/Model/User.swift
@@ -1,3 +1,4 @@
+
 //
 //  User.swift
 //  Eggong
@@ -20,3 +21,14 @@ struct User: FirebaseCodable, Identifiable {
         case profileImageURLString
     }
 }
+
+extension User {
+    static let dummy = User(
+        id: "J4TRZmkvNbUqPM9xT2NVOa6DZrw2",
+        bookmarkPlaces: ["LQXiQ1h77EJsmX7daDpG"],
+        favoritePlaces: [],
+        profileImageURLString: "https://firebasestorage.googleapis.com/v0/b/macc-eggong.appspot.com/o/Users%2FJ4TRZmkvNbUqPM9xT2NVOa6DZrw2%2FE019562A-6CEF-49D5-9A94-AF161E57EB35.jpg?alt=media&token=6b443e7e-b3df-4d28-8270-51645d9d5254"
+
+    )
+}
+
