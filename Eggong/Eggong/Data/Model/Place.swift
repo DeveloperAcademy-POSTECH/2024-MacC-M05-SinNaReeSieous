4ce6f82960643bf01ef3bf7ca775ff09c0de5944
YunHak Lee <lyh990408@gmail.com>
2024-10-20 14:51:03 +0900

    [Design] #9 - HomePlaceCardView UI 구현  완료

--- Eggong/Eggong/Data/Model/Place.swift
+++ Eggong/Eggong/Data/Model/Place.swift
@@ -26,3 +26,14 @@ struct Place: FirebaseCodable, Identifiable {
         case distance
     }
 }
+
+extension Place {
+    static let dummyPlace = Place(
+        name: "카페 휙",
+        keywords: ["따뜻한", "자유로운", "풋풋한"],
+        description: "서툴지만 한걸음씩 따뜻하게",
+        imageURLString: "https://firebasestorage.googleapis.com/v0/b/macc-eggong.appspot.com/o/Places%2FLQXiQ1h77EJsmX7daDpG%2F0C3A8BDF-FD28-41BD-BA23-390A2171A1AD.jpeg?alt=media&token=ae61edda-71ee-4714-a0f8-1c000382f70e",
+        uploadDate: Date(timeIntervalSinceNow: -5600),
+        distance: 300
+    )
+}
---
+++ Eggong/Eggong/Presentation/Home/HomeAssistantView/HomePlaceCardView.swift
@@ -0,0 +1,159 @@
+//
+//  HomePlaceCardView.swift
+//  Eggong
+//
+//  Created by YunhakLee on 10/19/24.
+//
+
+import SwiftUI
+
+import Kingfisher
+
+struct HomePlaceCardView: View {
+    @State var place: Place = Place.dummyPlace
+    @State var isBookmarked: Bool = true
+    @State var isFavorite: Bool = true
+    
+    let placeService: PlaceService = DefaultPlaceService()
+    let imageHorizontalPadding: CGFloat = 24
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년\nM월d일"
+        return formatter
+    }()
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            DateAndKeyword
+                .padding(.bottom, 16)
+            Thumbnail
+            HStack(alignment: .top) {
+                NameAndDescription
+                Spacer()
+                if isFavorite {
+                    FavoriteMark
+                        .offset(x: 12, y: 16)
+                }
+            }
+        }
+        .padding(8)
+        .background { RoundedRectangle(cornerRadius: 8).fill(Color(.brown50))}
+        .overlay(alignment: .topTrailing) {
+            if isBookmarked {
+                Bookmark
+                    .offset(x: -18, y: -10)
+            }
+        }
+        .padding(.horizontal, 16)
+        .task {
+            do {
+                self.place = try await placeService.getPlaces().first ?? Place.dummyPlace
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    
+}
+
+private extension HomePlaceCardView {
+    
+    // MARK: View
+    
+    var DateAndKeyword: some View {
+        HStack(alignment: .top) {
+            Text(dateFormatter.string(from: place.uploadDate))
+                .foregroundStyle(.black)
+                .font(.SDGothicNeo(size: 18, weight: .semibold))
+                .kerning(-18*0.02)
+                .frame(width: imageWidth/2, alignment: .leading)
+            Text(keywordText)
+                .foregroundStyle(.black)
+                .font(.SDGothicNeo(size: 18, weight: .semibold))
+                .kerning(-18*0.02)
+                .frame(width: imageWidth/2, alignment: .leading)
+        }
+    }
+    
+    var Thumbnail: some View {
+        KFImage(URL(string: place.imageURLString))
+            .resizable()
+            .scaledToFill()
+            .frame(width: imageWidth, height: imageHeight)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+    
+    var Name: some View {
+        VStack(alignment: .leading, spacing: -18) {
+            Text(placeName[0])
+                .foregroundStyle(.black)
+                .font(.SDGothicNeo(size: 48, weight: .extraBold))
+                .kerning(-48*0.02)
+            Text(placeName[1])
+                .foregroundStyle(.black)
+                .font(.SDGothicNeo(size: 48, weight: .extraBold))
+                .kerning(-48*0.02)
+        }
+    }
+    
+    var NameAndDescription: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Name
+            Text(place.description)
+                .foregroundStyle(.black)
+                .font(.SDGothicNeo(size: 12, weight: .extraBold))
+                .kerning(-12*0.02)
+        }
+    }
+    
+    var Bookmark: some View {
+        Image(systemName: "bookmark.fill")
+            .foregroundStyle(.accent)
+            .font(.system(size: 36, weight: .regular))
+    }
+    
+    var FavoriteMark: some View {
+        HStack(spacing: 4) {
+            Image(systemName: "heart.fill")
+                .foregroundStyle(.white)
+                .font(.system(size: 12, weight: .regular))
+            Text("아끼는 공간")
+                .foregroundStyle(.white)
+                .font(.SDGothicNeo(size: 10, weight: .extraBold))
+        }
+        .padding(.horizontal, 8)
+        .padding(.vertical, 4)
+        .background { Color.black }
+    }
+    
+    // MARK: Computed Values
+    
+    var imageWidth: CGFloat {
+        UIScreen.screenSize.width-imageHorizontalPadding*2
+    }
+    
+    var imageHeight: CGFloat {
+        imageWidth/345*349
+    }
+    
+    var keywordText: String {
+        let sortedKeywords = place.keywords.sorted(by: {$0.count < $1.count})
+        return sortedKeywords.joined(separator: "\n")
+    }
+    
+    var placeName: [String] {
+        if place.name.contains(where: {$0 == " "}) {
+            return place.name
+                .split{ $0 == " " }
+                .map{ String($0) }
+        } else {
+            return ["카페", place.name]
+        }
+    }
+}
+
+#Preview {
+    HomePlaceCardView()
+}
