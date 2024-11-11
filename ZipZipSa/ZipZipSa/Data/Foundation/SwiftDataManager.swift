5ee888dfc84567b59a6670dd5cb4e7c7b6a71a2b
YunHak Lee <lyh990408@gmail.com>
2024-11-11 18:51:14 +0900

    [Setting] #20 - Foundation 폴더에 파일 추가

---
+++ ZipZipSa/ZipZipSa/Data/Foundation/SwiftDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftDataManager.swift
+//  ZipZipSa
+//
+//  Created by YunhakLee on 11/11/24.
+//
+
+import Foundation
+
+class SwiftDataManager {
+    static let shared = SwiftDataManager()
+    private init() {}
+}
