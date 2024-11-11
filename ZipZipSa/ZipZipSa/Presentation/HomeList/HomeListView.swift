be8d258ee08285c651822016a75f094f97e6c679
YunHak Lee <lyh990408@gmail.com>
2024-11-11 21:19:14 +0900

    [Setting] #20 - Presentation 폴더에 파일 추가

---
+++ ZipZipSa/ZipZipSa/Presentation/HomeList/HomeListView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeListView.swift
+//  ZipZipSa
+//
+//  Created by YunhakLee on 11/11/24.
+//
+
+import SwiftUI
+
+struct HomeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeListView()
+}
---
+++ ZipZipSa/ZipZipSa/Presentation/Result/ResultCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultCardView.swift
+//  ZipZipSa
+//
+//  Created by YunhakLee on 11/11/24.
+//
+
+import SwiftUI
+
+struct ResultCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultCardView()
+}
