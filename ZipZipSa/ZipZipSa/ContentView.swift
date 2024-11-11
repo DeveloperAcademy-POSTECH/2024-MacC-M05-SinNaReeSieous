fe043197b6d07de812009cf324406af411e29ff1
YunHak Lee <lyh990408@gmail.com>
2024-11-11 18:23:05 +0900

    [Setting] #20 - 집집사, 새로운 시작

---
+++ ZipZipSa/ZipZipSa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZipZipSa
+//
+//  Created by YunhakLee on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZipZipSa/ZipZipSa/ZipZipSaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipZipSaApp.swift
+//  ZipZipSa
+//
+//  Created by YunhakLee on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ZipZipSaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
