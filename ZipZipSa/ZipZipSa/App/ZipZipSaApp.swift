56ae92d585f028b0bb06b3128bcace2e88043fac
Woohyun Jo <bluewing1004@gmail.com>
2024-11-20 18:02:09 +0900

    [Feat] #26 - GoogleMaps SPM 추가 및 App파일에 기본세팅

--- ZipZipSa/ZipZipSa/App/ZipZipSaApp.swift
+++ ZipZipSa/ZipZipSa/App/ZipZipSaApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import GoogleMaps
 
 @main
 struct ZipZipSaApp: App {
+    init() {
+        GMSServices.provideAPIKey(Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String ?? "API KEY를 가져오는데 실패했습니다.")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
