3e9500068063488437bf915f51b4d7cf5ae874e2
YunHak Lee <lyh990408@gmail.com>
2024-11-23 22:36:45 +0900

    [Chore] #31 - 체크리스트 데이터 모두 넣기 !

--- ZipZipSa/ZipZipSa/Data/Model/Checklist/ChecklistHazard.swift
+++ ZipZipSa/ZipZipSa/Data/Model/Checklist/ChecklistHazard.swift
@@ -16,17 +16,19 @@ enum Hazard: Hashable {
     case noise
     case privacy
     case cockroach
+    case cigaretteSmell
     
     var text: String {
         switch self {
-        case .waterLeak: return "누수위험"
-        case .waterPressure: return "수압위험"
-        case .waterCold: return "온수위험"
-        case .waterDrainage: return "배수위험"
-        case .mold: return "곰팡이위험"
-        case .noise: return "소음위험"
-        case .privacy: return "사생활위험"
-        case .cockroach: return "바퀴위험"
+        case .waterLeak: return "누수 위험"
+        case .waterPressure: return "수압 안좋음"
+        case .waterCold: return "온수 잘 안 나옴"
+        case .waterDrainage: return "배수 안좋음"
+        case .mold: return "곰팡이 위험"
+        case .noise: return "소음 위험"
+        case .privacy: return "사생활 위험"
+        case .cockroach: return "바퀴 위험"
+        case .cigaretteSmell: return "담배 위험"
         }
     }
 }
