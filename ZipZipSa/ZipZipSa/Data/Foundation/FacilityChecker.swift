e440a547ac9310fbb4b706990b00c7ee9da7b789
Woohyun Jo <bluewing1004@gmail.com>
2024-11-24 20:28:20 +0900

    [Fix] #26 - FacilityChecker 내 메서드 수정 및 에러처리부분 수정

--- ZipZipSa/ZipZipSa/Data/Foundation/FacilityChecker.swift
+++ ZipZipSa/ZipZipSa/Data/Foundation/FacilityChecker.swift
@@ -25,7 +25,7 @@ class FacilityChecker {
         try await withThrowingTaskGroup(of: (String, Bool).self) { group in
             for keyword in keywords {
                 group.addTask {
-                    let isFound = try await self.fetchFacility(for: keyword, at: location)
+                    let isFound = try await self.checkFacility(at: location, for: keyword)
                     return (keyword, isFound)
                 }
             }
@@ -40,22 +40,23 @@ class FacilityChecker {
     }
 
 
-    private func fetchFacility(for keyword: String, at location: CLLocationCoordinate2D) async throws -> Bool {
+    private func checkFacility(at location: CLLocationCoordinate2D, for keyword: String) async throws -> Bool {
         let urlString = ZipLiteral.APIEndpoints.nearbySearch(latitude: location.latitude, longitude: location.longitude, radius: radius, keyword: keyword, apiKey: apiKey).url
         
         guard let url = URL(string: urlString) else {
             throw NetworkError.invalidURL(keyword: keyword)
         }
         
-        let (data, _) = try await URLSession.shared.data(from: url)
-        
         do {
+            let (data, _) = try await URLSession.shared.data(from: url)
             let decoded = try JSONDecoder().decode(PlacesResponse.self, from: data)
             let filteredResults = decoded.results.filter {
                 self.isWithinRadius(center: location, location: $0.geometry.location, radius: self.radius)
             }
             return !filteredResults.isEmpty
-        } catch {
+        } catch let error as URLError {
+            throw NetworkError.networkError(keyword: keyword, description: error.localizedDescription)
+        } catch let error {
             throw NetworkError.decodingError(keyword: keyword, description: error.localizedDescription)
         }
     }
