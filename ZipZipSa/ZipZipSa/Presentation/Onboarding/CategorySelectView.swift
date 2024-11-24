e3a660166c4617931a7638586abd700c82047dc4
LEMONY <jin.lee1028@gmail.com>
2024-11-25 02:04:09 +0900

    [Design] #33 - CategorySelectView 진행상황 저장2
    버튼을 여러개 누르고 다시 해제했을 때 이전에 눌렀던 버튼의 메세지가 보이는 로직 구현중...

--- ZipZipSa/ZipZipSa/Presentation/Onboarding/CategorySelectView.swift
+++ ZipZipSa/ZipZipSa/Presentation/Onboarding/CategorySelectView.swift
@@ -8,25 +8,35 @@
 import SwiftUI
 
 struct CategorySelectView: View {
+    
+    @State private var totalTime: Int = 10
+    @State private var currentMessage: String = ""
+    
     var body: some View {
         NavigationStack{
             VStack {
-                HStack(alignment: .bottom) {
+                HStack(alignment: .bottom, spacing: 0) {
                     Image ("remark")
                         .resizable()
                         .frame(width: 62, height: 61)
                     
-                    Text("아래 카테고리에 해당하는 필수 질문은 이미 있어요. 중요하게 보고 싶은 카테고리를 선택하시면, 더 꼼꼼히 확인할 수 있도록 추가 질문도 챙겨드릴게요.")
-                        .padding(.horizontal, 16)
-                        .padding(.vertical, 12)
-                        .font(Font.system (size: 14, weight: .semibold))
-                        .background(Color.white)
-                        .overlay {
-                            UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 10, bottomTrailing: 10, topTrailing: 10))
-                                .stroke(.black, lineWidth: 1)
-                        }
+                    UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 10, bottomTrailing: 10, topTrailing: 10))
+                        .fill(Color.white)
+                        .stroke(.black, lineWidth: 1)
                         .padding(.leading, 18)
                         .padding(.trailing, 16)
+                        .overlay {
+                            Group {
+                                if currentMessage.isEmpty {
+                                    Text("아래 카테고리에 해당하는 필수 질문은 이미 있어요.\n중요하게 보고 싶은 카테고리를 선택하시면,\n더 꼼꼼히 확인할 수 있도록 추가 질문도 챙겨드릴게요.")
+                                } else {
+                                    Text(currentMessage)
+                                }
+                            }
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 12)
+                            .font(Font.system (size: 14, weight: .semibold))
+                        }
                 }
                 .padding(.leading, 24)
                 .padding(.bottom, 24)
@@ -39,20 +49,20 @@ struct CategorySelectView: View {
                             Text("집보는 시간")
                                 .font(Font.system (size: 13, weight: .medium))
                             
-                            Text("약 10분")
+                            Text("약 \(totalTime)분")
                                 .font(Font.system (size: 16, weight: .semibold))
                         }
                     }
                 
-                CategoryView()
+                CategoryView(totalTime: $totalTime, currentMessage: $currentMessage)
                 
                 NavigationLink(destination: CategorySelectView()) {
                     RoundedRectangle(cornerRadius: 12)
                         .fill(Color.mint)
                         .frame(width:UIScreen.screenSize.width - 32, height: 44)
                         .overlay {
-                                Text("완료")
-                                    .font(Font.system (size: 16, weight: .semibold))
+                            Text("완료")
+                                .font(Font.system (size: 16, weight: .semibold))
                         }
                 }
                 
@@ -63,9 +73,12 @@ struct CategorySelectView: View {
 }
 
 
+
 private extension CategorySelectView {
-            
-        }
+    
+    
+    
+}
 
 #Preview {
     CategorySelectView()
--- ZipZipSa/ZipZipSa/Presentation/Onboarding/CategoryView.swift
+++ ZipZipSa/ZipZipSa/Presentation/Onboarding/CategoryView.swift
@@ -6,54 +6,83 @@
 //
 
 import SwiftUI
-
 struct CategoryView: View {
+    
     private let categories: [Category] = [
-        Category(onImage: "InsectProofColor", offImage: "InsectProofSepia"),
-        Category(onImage: "CleanlinessColor", offImage: "CleanlinessSepia"),
-        Category(onImage: "SecurityColor", offImage: "SecuritySepia"),
-        Category(onImage: "VentilationColor", offImage: "VentilationSepia"),
-        Category(onImage: "SoundproofColor", offImage: "SoundproofSepia"),
-        Category(onImage: "LightedColor", offImage: "LightedSepia")
+        Category(onImage: "InsectProofColor", offImage: "InsectProofSepia", requiredTime: 3, categoryMessage: "해충 흔적과 방충 시설 상태를 더 꼼꼼히 볼 수 있도록 질문을 추가해둘게요."),
+        Category(onImage: "CleanlinessColor", offImage: "CleanlinessSepia", requiredTime: 3, categoryMessage: "집 안에서 놓치기 쉬운 곳까지 구석구석 살펴볼 수 있도록 질문을 추가해둘게요."),
+        Category(onImage: "SecurityColor", offImage: "SecuritySepia", requiredTime: 3, categoryMessage: "보안 장치, 주변 환경, 시설까지 꼼꼼히 살필 수 있게 질문을 추가해둘게요."),
+        Category(onImage: "VentilationColor", offImage: "VentilationSepia", requiredTime: 3, categoryMessage: "집이 환기하기에 좋은 상태인지 알 수 있도록 체크 포인트를 알려드릴게요!"),
+        Category(onImage: "SoundproofColor", offImage: "SoundproofSepia", requiredTime: 3, categoryMessage: "이 집이 소음, 방음 측면에서 어떤지 더 꼼꼼히 알 수 있도록 질문을 추가해둘게요."),
+        Category(onImage: "LightedColor", offImage: "LightedSepia", requiredTime: 3, categoryMessage: "집 안에 빛이 잘 들어오는지 좀 더 꼼꼼히 체크할 수 있도록 할게요.")
     ]
     
+    @Binding var totalTime: Int
+    @Binding var currentMessage: String
     @State private var selectedCategories: Set<String> = []
+    @State private var selectionOrder: [String] = [] // 선택 순서를 추적하기 위한 배열
     
     var body: some View {
-        LazyVGrid(columns: [
-            GridItem(.flexible()),
-            GridItem(.flexible())
-        ], spacing: 0) {
-            ForEach(categories, id: \.offImage) { category in
-                categoryButton(for: category)
+        VStack {
+            LazyVGrid(columns: [
+                GridItem(.flexible(), spacing: 11),
+                GridItem(.flexible(), spacing: 11)
+            ], spacing: 11) {
+                ForEach(categories, id: \.offImage) { category in
+                    CategoryButton(for: category)
+                }
+            }
+            .padding(.horizontal, 16)
+        }
+    }
+}
+
+private extension CategoryView {
+    
+    func CategoryButton(for category: Category) -> some View {
+        let isSelected = selectedCategories.contains(category.offImage)
+        
+        return Button(action: {
+            toggleCategory(category)
+        }) {
+            VStack {
+                Image(isSelected ? category.offImage : category.onImage)
+                    .resizable()
+                    .scaledToFit()
             }
         }
     }
     
-    private func categoryButton(for category: Category) -> some View {
-        let isSelected = selectedCategories.contains(category.onImage)
-        
-        return Button(action: {
-            if isSelected {
-                selectedCategories.remove(category.onImage)
+    func toggleCategory(_ category: Category) {
+            if selectedCategories.contains(category.offImage) {
+                // 카테고리 선택 해제
+                selectedCategories.remove(category.offImage)
+                totalTime -= category.requiredTime
+                selectionOrder.removeAll(where: { $0 == category.offImage })
+                
+                if selectedCategories.isEmpty {
+                    // 선택된 카테고리가 없을 때 메시지 초기화
+                    currentMessage = ""
+                } else {
+                    // 마지막으로 선택된 카테고리의 메시지를 표시
+                    if let lastSelectedOffImage = selectionOrder.last,
+                       let lastCategory = categories.first(where: { $0.offImage == lastSelectedOffImage }) {
+                        currentMessage = lastCategory.categoryMessage
+                    }
+                }
             } else {
-                selectedCategories.insert(category.onImage)
+                // 새로운 카테고리 선택
+                selectedCategories.insert(category.offImage)
+                totalTime += category.requiredTime
+                selectionOrder.append(category.offImage)
+                currentMessage = category.categoryMessage
             }
-        }) {
-            VStack {
-                Image(isSelected ? category.onImage : category.offImage)
-                    .aspectRatio(contentMode: .fit)
-            }
-            .padding(.top, 12)
         }
-        .foregroundColor(.black)
     }
-}
 
 struct Category {
     let onImage: String
     let offImage: String
-}
-#Preview {
-    CategoryView()
+    let requiredTime: Int
+    let categoryMessage: String
 }
