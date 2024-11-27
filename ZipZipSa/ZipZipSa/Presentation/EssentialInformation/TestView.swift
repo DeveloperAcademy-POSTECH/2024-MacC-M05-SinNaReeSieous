cb5f2ae2dadaf18eb4d5ebeb5d81a309b6369c0d
YunHak Lee <lyh990408@gmail.com>
2024-11-28 02:39:38 +0900

    [Design] #42 - 집 주소 섹션 구현 완료

--- ZipZipSa/ZipZipSa/Presentation/EssentialInformation/TestView.swift
+++ ZipZipSa/ZipZipSa/Presentation/EssentialInformation/TestView.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct TestView: View {
-    @State var houseName: String = ""
+    @State var homeName: String = ""
+    @State var address: String = ""
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 32) {
                 NavigationTitle
-                HouseName
+                NameSection
+                AddressSection
             }
             .padding(.horizontal, 16)
         }
@@ -34,11 +36,32 @@ private extension TestView {
             .applyZZSFont(zzsFontSet: .largeTitle)
     }
     
-    var HouseName: some View {
+    var NameSection: some View {
         VStack(alignment: .leading, spacing: 8) {
             SectionTitle(text: "집 별명")
-            TextField(text: $houseName) {
-                Text("\(basicHouseName)")
+            TextField(text: $homeName) {
+                Text(basicHouseName)
+                    .foregroundStyle(Color.Text.placeholder)
+                    .applyZZSFont(zzsFontSet: .bodyRegular)
+            }
+            .foregroundStyle(Color.Text.primary)
+            .applyZZSFont(zzsFontSet: .bodyRegular)
+            .padding(.horizontal, 20)
+            .padding(.vertical, 8)
+            .background {
+                UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(bottomLeading: 16,
+                                                                         bottomTrailing: 16,
+                                                                         topTrailing: 16))
+                .fill(Color.Button.enable)
+            }
+        }
+    }
+    
+    var AddressSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            SectionTitle(text: "주소")
+            TextField(text: $address) {
+                Text("주소를 입력해 주세요")
                     .foregroundStyle(Color.Text.placeholder)
                     .applyZZSFont(zzsFontSet: .bodyRegular)
             }
