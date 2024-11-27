8e2598d2252fe1a6accfe0094dfadbfa88d8bf47
LEMONY <jin.lee1028@gmail.com>
2024-11-27 19:03:20 +0900

    [Design] #33 - HomeListView 구현

--- ZipZipSa/ZipZipSa/Presentation/HomeList/HomeListView.swift
+++ ZipZipSa/ZipZipSa/Presentation/HomeList/HomeListView.swift
@@ -7,9 +7,25 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct HomeListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color.Background.primary
+                .ignoresSafeArea()
+            VStack(alignment: .leading) {
+                Text("내가 본 집")
+                    .applyZZSFont(zzsFontSet: .largeTitle)
+                
+                Image("cryingYongboogiSquareColor")
+                
+                Text("아직 내가 둘러본 집이 없어요.\n집을 보러 가서 집을 추가해 보세요.")
+                
+                
+                
+            }
+        }
     }
 }
 
