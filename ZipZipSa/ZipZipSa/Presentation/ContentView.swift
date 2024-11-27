6dc32675eb2b295899319a90684e02d0ed58b386
Woohyun Jo <bluewing1004@gmail.com>
2024-11-28 00:38:30 +0900

    [Fix] #40 - ContentView develop기준으로 되돌리기

--- ZipZipSa/ZipZipSa/Presentation/ContentView.swift
+++ ZipZipSa/ZipZipSa/Presentation/ContentView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            NavigationLink(destination: RoomScanView()) {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
