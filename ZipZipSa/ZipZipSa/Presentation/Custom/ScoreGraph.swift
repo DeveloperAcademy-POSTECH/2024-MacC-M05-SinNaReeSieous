9509bb270535259d1a17f044586213c7fe0d100f
Woohyun Jo <bluewing1004@gmail.com>
2024-11-29 04:25:40 +0900

    [Design] #50 - ShareCardView의 ChecklistResult 구현 및 ScoreGraph 컴포넌트화

---
+++ ZipZipSa/ZipZipSa/Presentation/Custom/ScoreGraph.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreGraph.swift
+//  ZipZipSa
+//
+//  Created by 조우현 on 11/29/24.
+//
+
+import SwiftUI
+
+struct ScoreGraph: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScoreGraph()
+}
