0161c9094bc16d6ad7f09005c953558cb5184225
Woohyun Jo <bluewing1004@gmail.com>
2024-11-29 02:32:37 +0900

    [Add] #50 - ZZSTag라는 이름으로 ResultCardView에 들어가는 태크 컴포넌트화

---
+++ ZipZipSa/ZipZipSa/Presentation/Custom/ZZSTag.swift
@@ -0,0 +1,18 @@
+//
+//  ZZSTag.swift
+//  ZipZipSa
+//
+//  Created by 조우현 on 11/29/24.
+//
+
+import SwiftUI
+
+struct ZZSTag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZZSTag()
+}
