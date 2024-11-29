bb51b2135535b8924425e9c798f79031e8321475
Woohyun Jo <bluewing1004@gmail.com>
2024-11-30 03:06:23 +0900

    [Fix] #66 - FullScreenCover로 올라왔을 때 모델을 캡쳐할 수 있도록 수정
    잘가라 PathModel

--- ZipZipSa/ZipZipSa/Presentation/RoomScan/RoomScanAssistantView/DoneScanningView.swift
+++ ZipZipSa/ZipZipSa/Presentation/RoomScan/RoomScanAssistantView/DoneScanningView.swift
@@ -110,17 +110,20 @@ private extension DoneScanningView {
     
     //캡쳐된 모델이 보이는 뷰를 이미지로 캡쳐하기 위한 함수
     func captureScreen() {
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            if let window = windowScene.windows.first(where: { $0.isKeyWindow }) {
-                let rootView = window.rootViewController?.view
-                let topMargin: CGFloat = 100
-                let bottomMargin: CGFloat = 300
-                let screenHeight = UIScreen.main.bounds.height
-                let screenWidth = UIScreen.main.bounds.width
-                let rect = CGRect(x: 0, y: topMargin, width: screenWidth, height: screenHeight - topMargin - bottomMargin)
-                capturedView = rootView?.snapshot(of: rect)
-            }
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first(where: { $0.isKeyWindow }),
+              let topViewController = window.topMostViewController() else {
+            print("Top view controller not found")
+            return
         }
+
+        let topMargin: CGFloat = 100
+        let bottomMargin: CGFloat = 300
+        let screenHeight = UIScreen.main.bounds.height
+        let screenWidth = UIScreen.main.bounds.width
+        let rect = CGRect(x: 0, y: topMargin, width: screenWidth, height: screenHeight - topMargin - bottomMargin)
+
+        capturedView = topViewController.view.snapshot(of: rect)
     }
     
     //이미지의 배경을 제거하는 함수
