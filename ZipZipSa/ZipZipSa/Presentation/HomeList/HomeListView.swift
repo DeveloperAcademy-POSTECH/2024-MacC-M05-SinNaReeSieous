7df8f768cc0ef8d65952e8c6c4b1249275acb785
LEMONY <jin.lee1028@gmail.com>
2024-11-30 01:18:19 +0900

    [Design] #55 - ViewedHomeCell 수정사항 반영

--- ZipZipSa/ZipZipSa/Presentation/HomeList/HomeListView.swift
+++ ZipZipSa/ZipZipSa/Presentation/HomeList/HomeListView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct HomeListView: View {
     @State private var homeList: [ViewedHome] = [
-                ViewedHome(image: "mainPic_sample", title: "첫 번째 집", address: "부산광역시 강서구 녹산산단 382로 10~29번지 (송정동)"),
-                ViewedHome(image: "mainPic_sample", title: "두 번째 집", address: "서울시 강동구 동남로78길 48 (고덕1동)")
+        ViewedHome(image: "mainPic_sample", title: "첫 번째 집", address: "부산광역시 강서구 녹산산단 382로 10~29번지 (송정동)", rentType: "월세"),
+                ViewedHome(image: "mainPic_sample", title: "두 번째 집", address: "서울시 강동구 동남로78길 48 (고덕1동)", rentType: "전세")
     ]  // 데이터 모델을 위한 배열
     
     @State private var showHomeHuntSheet = false
