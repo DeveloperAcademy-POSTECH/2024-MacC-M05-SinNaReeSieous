c36ca4092c3bf59dd218888dfbfbbcdba194874f
LEMONY <jin.lee1028@gmail.com>
2024-12-30 19:34:04 +0900

    [Fix] #119 - OnboardingView 및 SettingView 카테고리 시간 변경

--- ZipZipSa/ZipZipSa/Data/Model/OnboardingCategory.swift
+++ ZipZipSa/ZipZipSa/Data/Model/OnboardingCategory.swift
@@ -24,7 +24,7 @@ extension OnboardingCategory {
             categoryElement: "InsectProof",
             onImage: "InsectProofColor",
             offImage: "InsectProofSepia",
-            requiredTime: 3,
+            requiredTime: 1,
             categoryMessage: "해충 흔적과 방충 시설 상태를 더 꼼꼼히 볼 수 있도록 질문을 추가해둘게요."
         ),
         OnboardingCategory(
@@ -32,7 +32,7 @@ extension OnboardingCategory {
             categoryElement: "Cleenliness",
             onImage: "CleanlinessColor",
             offImage: "CleanlinessSepia",
-            requiredTime: 3,
+            requiredTime: 2,
             categoryMessage: "집 안에서 놓치기 쉬운 곳까지 구석구석 살펴볼 수 있도록 질문을 추가해둘게요."
         ),
         OnboardingCategory(
@@ -40,7 +40,7 @@ extension OnboardingCategory {
             categoryElement: "Security",
             onImage: "SecurityColor",
             offImage: "SecuritySepia",
-            requiredTime: 3,
+            requiredTime: 1,
             categoryMessage: "보안 장치, 주변 환경, 시설까지 꼼꼼히 살필 수 있게 질문을 추가해둘게요."
         ),
         OnboardingCategory(
@@ -48,7 +48,7 @@ extension OnboardingCategory {
             categoryElement: "Ventilation",
             onImage: "VentilationColor",
             offImage: "VentilationSepia",
-            requiredTime: 3,
+            requiredTime: 1,
             categoryMessage: "집이 환기하기에 좋은 상태인지 알 수 있도록 체크 포인트를 알려드릴게요!"
         ),
         OnboardingCategory(
@@ -56,7 +56,7 @@ extension OnboardingCategory {
             categoryElement: "Soundproof",
             onImage: "SoundproofColor",
             offImage: "SoundproofSepia",
-            requiredTime: 3,
+            requiredTime: 1,
             categoryMessage: "이 집이 소음, 방음 측면에서 어떤지 더 꼼꼼히 알 수 있도록 질문을 추가해둘게요."
         ),
         OnboardingCategory(
@@ -64,7 +64,7 @@ extension OnboardingCategory {
             categoryElement: "Lighted",
             onImage: "LightedColor",
             offImage: "LightedSepia",
-            requiredTime: 3,
+            requiredTime: 1,
             categoryMessage: "집 안에 빛이 잘 들어오는지 좀 더 꼼꼼히 체크할 수 있도록 할게요.")
     ]
 }
